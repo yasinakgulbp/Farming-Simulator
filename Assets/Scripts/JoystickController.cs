464a8cf895cb499958dcb3903171620d23fb81cd
Yasin Akgül <62186511+yasinakgulbp@users.noreply.github.com>
2024-02-07 03:41:33 +0300

    Fİrst Commit

---
+++ Assets/Scripts/JoystickController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickController : MonoBehaviour
+{
+
+    [SerializeField] private RectTransform joystickOutline;
+    [SerializeField] private RectTransform joystickButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ekrana dokunulduğunu algılayıp, joystick'in görünmesini sağlayacağız
+    }
+
+
+    public void TappedOnJoystickZone()
+    {
+
+    }
+
+    private void ShowJoystick()
+    {
+
+    }
+
+    private void HideJoystick()
+    {
+
+    }
+
+    public void ControlJoystick()
+    {
+        //Joystick ile karakterimizi kontrol edeceğiz
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
