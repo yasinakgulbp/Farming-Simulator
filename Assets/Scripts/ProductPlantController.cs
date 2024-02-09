b886b9031e2f37ca45f9612dd9dd887df42989eb
Yasin Akgül <62186511+yasinakgulbp@users.noreply.github.com>
2024-02-10 00:01:00 +0300

    Pastaneden gelen paralar alınıp harcanabiliyor ve prefab haline getirildi

--- Assets/Scripts/ProductPlantController.cs
+++ Assets/Scripts/ProductPlantController.cs
@@ -42,7 +42,7 @@ public class ProductPlantController : MonoBehaviour
         Vector3 targetScale = originalScale / 3;
         transform.gameObject.LeanScale(targetScale, 1f);
 
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(10f);
 
         transform.gameObject.LeanScale(originalScale, 1f).setEase(LeanTweenType.easeOutBack);
         isReadyToPick=true;
--- Assets/Scripts/UnlockBakeryUnitController.cs
+++ Assets/Scripts/UnlockBakeryUnitController.cs
@@ -9,6 +9,14 @@ public class UnlockBakeryUnitController : MonoBehaviour
     [SerializeField] private int maxStoredProductCount;
     [SerializeField] private ProductType productType;
     private int storedProductCount;
+
+    [SerializeField] private int UseProductInSecconds = 0;
+    [SerializeField] private Transform coinTransform;
+    [SerializeField] private GameObject coinGo;
+    private float time;
+
+    [SerializeField] private ParticleSystem smokeParticle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +26,23 @@ public class UnlockBakeryUnitController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (storedProductCount > 0)
+        {
+            time += Time.deltaTime;
+
+            if (time >= UseProductInSecconds)
+            {
+                time = 0.0f;
+                UseProduct();
+            }
+        }
         
     }
 
     private void DisplayProductCount()
     {
         bakeryText.text = storedProductCount.ToString() + "/" + maxStoredProductCount.ToString();
+        ControlSmokeEffect();
     }
 
     public ProductType GetNeededProductType()
@@ -41,5 +60,38 @@ public class UnlockBakeryUnitController : MonoBehaviour
         storedProductCount++;
         DisplayProductCount();
         return true;
+    }
+
+    private void UseProduct()
+    {
+        storedProductCount--;
+        DisplayProductCount();
+        CreateCoin();
+    }
+
+    private void CreateCoin()
+    {
+        Vector3 position = Random.insideUnitSphere * 1f;
+        Vector3 instantiatePos = coinTransform.position + position;
+
+        Instantiate(coinGo, instantiatePos, Quaternion.identity);
+    }
+
+    private void ControlSmokeEffect()
+    {
+        if (storedProductCount == 0)
+        {
+            if (smokeParticle.isPlaying)
+            {
+                smokeParticle.Stop();
+            }
+        }
+        else
+        {
+            if (smokeParticle.isStopped) 
+            { 
+                smokeParticle.Play();
+            }
+        }
     }
 }
